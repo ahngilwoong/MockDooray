44a06eed82cea76c625f50cb2cbff7b7328ef155
ahngilwoong <o_rea@naver.com>
2022-08-01 19:42:38 +0900

    .

--- Gateways/GatewayServer/src/main/java/com/nhnacademy/gatewayserver/adaptor/impl/TaskAdaptorImpl.java
+++ Gateways/GatewayServer/src/main/java/com/nhnacademy/gatewayserver/adaptor/impl/TaskAdaptorImpl.java
@@ -3,10 +3,20 @@ package com.nhnacademy.gatewayserver.adaptor.impl;
 import com.nhnacademy.gatewayserver.adaptor.TaskAdaptor;
 import com.nhnacademy.gatewayserver.request.TaskRequest;
 import com.nhnacademy.gatewayserver.response.TaskResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+@Component
+@RequiredArgsConstructor
 public class TaskAdaptorImpl implements TaskAdaptor {
+
+    private final RestTemplate restTemplate;
+    private HttpHeaders httpHeaders;
+
     @Override
     public List<TaskResponse> findProjectInTasks(Long taskNum) {
         return null;
